a1d5860e4130685675863274195a37be3a4f31dc
Scott Yargeau <scottyargeau@gmail.com>
2017-10-08 14:57:13 -0400

    Redirect after POST, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -17,11 +17,37 @@ class HomePageTest(TestCase):
 
     def test_save_post_request(self):
         """
-        Tests that a post request gets submitted and saved successfully.
+        Tests that a post request gets saved in database.
         """
         response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertIn('A new list item', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirect_after_post(self):
+        """
+        Tests that a post is redirected to home
+        """
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_dont_save_empty_item(self):
+        """
+        Tests that empty posts aren't always saved to database. Save only when item is posted.
+        """
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_display_all_list_items(self):
+        """
+        Test that all list items are displayed on home page.
+        """
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+        response = self.client.get('/')
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
 
 
 class ItemModelTest(TestCase):
