564d9ce0523744785bcef34137e54e4842112b64
Scott Yargeau <scottyargeau@gmail.com>
2017-10-18 23:12:42 -0400

    Use get_absolute_url on List model to remove hard-coded URLs in views

--- lists/models.py
+++ lists/models.py
@@ -1,11 +1,17 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 
 class List(models.Model):
     """
     Handles storing the different lists
     """
-    pass
+    
+    def get_absolute_url(self):
+        """
+        Returns the URL used by the view
+        """
+        return reverse('view_list', args=[self.id])
 
 
 class Item(models.Model):
