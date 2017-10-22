0c3543621039a90c8ac8d6f5f987d5a67395b5ba
Scott Yargeau <scottyargeau@gmail.com>
2017-10-22 15:58:09 -0400

    Duplicate item validation form; view test fails

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.forms import (
+    ItemForm, ExistingListItemForm,
+    EMPTY_ITEM_ERROR, DUPLICATE_ITEM_ERROR
+)
 from lists.models import Item, List
 
 
@@ -37,3 +40,42 @@ class ItemFormTest(TestCase):
         self.assertEqual(new_item, Item.objects.first())
         self.assertEqual(new_item.text, 'do me')
         self.assertEqual(new_item.list, list_)
+
+
+class ExistingListItemFormTest(TestCase):
+    """
+    Tests the form for existing list items.
+    """
+
+    def test_form_renders_item_text_input(self):
+        """
+        Test that the form has the proper placeholder in the text box.
+        """
+        list_ = List.objects.create()
+        form = ExistingListItemForm(for_list=list_)
+        self.assertIn('placeholder="Enter a to-do item"', form.as_p())
+    
+    def test_form_validation_for_blank_items(self):
+        """
+        Test that the form validates for blank items and returns the correct error message.
+        """
+        list_ = List.objects.create()
+        form = ExistingListItemForm(for_list=list_, data={'text': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(
+            form.errors['text'],
+            [EMPTY_ITEM_ERROR]
+        )
+
+    def test_form_validation_for_duplicate_items(self):
+        """
+        Tests that the form validates for duplicate items and returns the correct error message.
+        """
+        list_ = List.objects.create()
+        Item.objects.create(list=list_, text='no twins!')
+        form = ExistingListItemForm(for_list=list_, data={'text': 'no twins!'})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(
+            form.errors['text'],
+            [DUPLICATE_ITEM_ERROR]
+        )
