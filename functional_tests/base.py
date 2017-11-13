463ad002e08e6f20dc284a6503ebe69ac9aeb990
Scott Yargeau <scottyargeau@gmail.com>
2017-11-12 19:40:55 -0500

    Create user session using manage command locally and on server

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -3,6 +3,7 @@ import os
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
+from .server_tools import reset_database
 
 MAX_WAIT = 10
 
@@ -31,9 +32,10 @@ class FunctionalTest(StaticLiveServerTestCase):
         Starts the web browser.
         """
         self.browser = webdriver.Firefox()
-        staging_server = os.environ.get('STAGING_SERVER')
-        if staging_server:
-            self.live_server_url = 'http://' + staging_server
+        self.staging_server = os.environ.get('STAGING_SERVER')
+        if self.staging_server:
+            self.live_server_url = 'http://' + self.staging_server
+            reset_database(self.staging_server)
 
     def tearDown(self):
         """
