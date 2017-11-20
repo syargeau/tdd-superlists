3fbcf47051f173c36f1341c58d011a581b57ca65
Scott Yargeau <scottyargeau@gmail.com>
2017-11-20 01:42:08 -0500

    Create page obects for list pages, use in sharing FT

--- functional_tests/test_sharing.py
+++ functional_tests/test_sharing.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from .base import FunctionalTest
+from .list_page import ListPage
+from .my_lists_page import MyListsPage
 
 
 def quit_if_possible(browser):
@@ -26,11 +28,35 @@ class SharingTest(FunctionalTest):
         # Bob goes to the home page and starts a list
         self.browser = bob_browser
         self.browser.get(self.live_server_url)
-        self.add_list_item('Get help')
+        list_page = ListPage(self).add_list_item('Get help')
 
         # He notices a "Share this list" option
-        share_box = self.browser.find_element_by_css_selector('input[name="sharee"]')
+        share_box = list_page.get_share_box()
         self.assertEqual(
             share_box.get_attribute('placeholder'),
             'your-friend@example.complex'
         )
+
+        # He shares his list. The page updates stating it was shared with Tom.
+        list_page.share_list_with('tom@example.com')
+
+        # Tom now goes to his list page
+        self.browser = tom_browser
+        MyListsPage(self).go_to_my_lists_page()
+
+        # He sees Bob's list in there!
+        self.browser.find_element_by_link_text('Get help').click()
+        self.wait_for(
+            lambda: self.assertEqual(
+                list_page.get_list_owner(),
+                'bob@example.com'
+            )
+        )
+
+        # He adds an item to Bob's list
+        list_page.add_list_item('Hi Bob!')
+
+        # When Bob refreshes the page, he sees Tom's addition
+        self.browser = bob_browser
+        self.browser.refresh()
+        list_page.wait_for_item_in_list('Hi Bob!', 2)
